5fe42803675cfc72e960e2422ea1381117295017
Filip Mattsson <famattsson@gmail.com>
2018-04-17 00:10:53 +0200

    First build of rider

---
+++ Rider Replica/Assets/CarController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    bool isMoving = false;
+    public Rigidbody2D rb;
+    public float speed = 20;
+    public float rotSpeed = 2;
+    public static bool isGrounded = true;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            isMoving = true;
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            isMoving = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            if (isGrounded)
+                rb.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            else
+                rb.AddTorque(rotSpeed * rotSpeed * Time.fixedDeltaTime * 100f);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
---
+++ Rider Replica/Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    GameObject player;
+    bool gameHasEnded = false;
+    public GameObject GameOverMenu;
+    public Transform spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((player.transform.position.y <= -20 && !gameHasEnded) || 
+            (Vector3.Dot(player.transform.up, Vector3.down) > 0.3 && !gameHasEnded && CarController.isGrounded))
+        {
+            gameHasEnded = true;
+            GameOver();
+        }
+	}
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f;
+        GameOverMenu.SetActive(true);
+    }
+    public void Retry ()
+    {
+        CarController.isGrounded = false;
+        gameHasEnded = false;
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        rb.rotation = 0f;
+        rb.angularVelocity = 0f;
+        rb.velocity = new Vector2(0f,0f);
+        Time.timeScale = 1f;
+        GameOverMenu.SetActive(false);
+        player.transform.position = spawnPoint.position;
+    }
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+}
